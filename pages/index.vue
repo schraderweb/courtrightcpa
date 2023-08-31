965e685aa9958ede2db34f351c0d4bd853dc5f22
Mian Yasir Iqbal <yasiriqbal1990@gmail.com>
2023-08-31 16:42:27 +0500

    Updated

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 
 <header>
    <div class="main">
-    <h1>COMMING SOON!</h1>
+    <h1>COMING SOON!</h1>
     <h3>Courtright Accounting Solutions LLC</h3>
    </div>
     </header>
@@ -25,9 +25,9 @@
            <!-- Call Us Section -->
            <div class="contact-section">
             <div class="icons-section">
-               <a href="">
-                <svg style="color: white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone-call"><path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" fill="white"></path></svg>
-                <span class="" >Call Us </span>
+               <a href="tel:2314091834">
+                <svg style="color: white" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone-call"><path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" fill="white"></path></svg>
+                <span class="" >Click To Call Us </span>
                 </a>
             </div>
        
